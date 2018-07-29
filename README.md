b72ff8dd3c91dbac75dd4177dc706d29da55307f
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-07-29 10:59:00 +0200

    Updated the interface.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## todos
 
 - Add a terminal interface with `curser` or `urwid`
+- Add gmaven repository, see [here](https://developer.android.com/studio/build/dependencies#gmaven-access)
--- qmaven/maven.py
+++ qmaven/maven.py
@@ -6,3 +6,8 @@ class MavenAPI:
     def search(query: str) -> requests.Response:
         url = "http://search.maven.org/solrsearch/select?q={query}&rows=20&wt=json".format(query=query)
         return requests.get(url)
+
+    @staticmethod
+    def gid(gid: str) -> requests.Response:
+        url = "http://search.maven.org/solrsearch/select?q=g:{gid}&rows=20&wt=json".format(gid=gid)
+        return requests.get(url)
